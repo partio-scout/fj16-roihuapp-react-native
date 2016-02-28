13aadd9287857bad21e70ac27c69467a2e7263d9
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-02-28 20:21:18 +0200

    Add "Auth" component for displaying Login view when needed

---
+++ RoihuApp/components/auth/index.js
@@ -0,0 +1,32 @@
+'use strict';
+import React, {
+  Component,
+  View
+} from 'react-native';
+import { connect } from 'react-redux';
+import Login from '../login/index.js';
+
+class Auth extends Component {
+  render() {
+    const { token } = this.props;
+    return (
+      <View style={{flex: 1}}>{this.renderChildren(token)}</View>
+    );
+  }
+
+  renderChildren(token) {
+    if (token === "") {
+      return (
+        <Login/>
+      );
+    } else {
+      return (
+        this.props.children
+      );
+    }
+  }
+}
+
+export default connect(state => ({
+  token: state.token
+}))(Auth);
