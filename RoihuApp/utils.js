3dd275cf161b4928a940e86713eb9253f1fcfae4
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-04-29 21:33:57 +0300

    Add utils.js missing from last commit

---
+++ RoihuApp/utils.js
@@ -0,0 +1,14 @@
+'use strict';
+import React, {
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+export function renderBackButton(navigator) {
+  return (
+    <TouchableOpacity style={navigationStyles.backButton}
+                      onPress={() => navigator.pop()}>
+      <Text>Takaisin</Text>
+    </TouchableOpacity>
+  );
+}
