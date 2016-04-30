45c2a6b7e38ee5e71b8bd2abf9821a2a033a767b
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-04-30 08:37:48 +0300

    Remove iOS test that looks for welcome text

--- RoihuApp/ios/RoihuAppTests/RoihuAppTests.m
+++ RoihuApp/ios/RoihuAppTests/RoihuAppTests.m
@@ -39,7 +39,6 @@
 {
   UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
   NSDate *date = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
-  BOOL foundElement = NO;
 
   __block NSString *redboxError = nil;
   RCTSetLogFunction(^(RCTLogLevel level, RCTLogSource source, NSString *fileName, NSNumber *lineNumber, NSString *message) {
@@ -48,22 +47,14 @@
     }
   });
 
-  while ([date timeIntervalSinceNow] > 0 && !foundElement && !redboxError) {
+  while ([date timeIntervalSinceNow] > 0 && !redboxError) {
     [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
     [[NSRunLoop mainRunLoop] runMode:NSRunLoopCommonModes beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-
-    foundElement = [self findSubviewInView:vc.view matching:^BOOL(UIView *view) {
-      if ([view.accessibilityLabel isEqualToString:TEXT_TO_LOOK_FOR]) {
-        return YES;
-      }
-      return NO;
-    }];
   }
 
   RCTSetLogFunction(RCTDefaultLogFunction);
 
   XCTAssertNil(redboxError, @"RedBox error: %@", redboxError);
-  XCTAssertTrue(foundElement, @"Couldn't find element with text '%@' in %d seconds", TEXT_TO_LOOK_FOR, TIMEOUT_SECONDS);
 }
 
 
