93f8dbfafea5fef86826ed7514c1ff2ee08fbbde
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-05-01 22:08:42 +0300

    Add scaffolding for displaying achievements

--- RoihuApp/components/achievements/index.js
+++ RoihuApp/components/achievements/index.js
@@ -3,18 +3,108 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  Dimensions,
+  Navigator,
+  TouchableOpacity,
+  Platform,
+  BackAndroid
 } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { config } from '../../config.js';
+import { renderBackButton } from '../../utils.js';
+
+var _navigator;
+
+if (Platform.OS === 'android') {
+  BackAndroid.addEventListener('hardwareBackPress', () => {
+    if (!_navigator) {
+      return false;
+    }
+    if (_navigator.getCurrentRoutes().length === 1  ) {
+      return false;
+    }
+    _navigator.pop();
+    return true;
+  });
+}
 
 class Achievements extends Component {
+
+  renderAgelevel(agelevel, navigator) {
+    return (
+      <View key={"agelevel-" + agelevel.id} style={{padding: 10}}>
+        <TouchableOpacity onPress={() => {
+            this.props.actions.selectAgelevel(agelevel);
+            navigator.push({name: "achievements"});
+          }}>
+          <Text>{agelevel.title}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  renderAgelevels(navigator) {
+    return (
+      <View style={{flex: 1}}>
+        <TouchableOpacity style={{padding: 10}}
+                          onPress={() => this.fetchAchievements()}>
+          <Text>Päivitä</Text>
+        </TouchableOpacity>
+        <ListView key={"agelevels"}
+                  dataSource={this.props.ageLevelDataSource}
+                  renderRow={(agelevel) => this.renderAgelevel(agelevel, navigator)}/>
+      </View>
+    );
+  }
+
+  renderAchievement(achievement, navigator) {
+    return (
+      <View key={"achievement-" + achievement.title} style={{padding: 10}}>
+        <TouchableOpacity onPress={() => {
+            this.props.actions.selectAchievement(achievement);
+            navigator.push({name: "achievement"});
+          }}>
+          <Text>{achievement.title}</Text>
+          <Text>{achievement.bodytext}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  renderAchievements(navigator) {
+    return (
+      <View style={{flex: 1}}>
+        {Platform.OS !== 'android' ? renderBackButton(navigator) : null }
+        <ListView key={"achievements"}
+                  enableEmptySections={true}
+                  dataSource={this.props.achievementsDataSource}
+                  renderRow={(achievement) => this.renderAchievement(achievement, navigator)}/>
+      </View>
+    );
+  }
+
+  renderScene(route, navigator) {
+    _navigator = navigator;
+    switch(route.name) {
+    case "achievements":
+      return this.renderAchievements(navigator);
+    case "agelevels":
+    default:
+      return this.renderAgelevels(navigator);
+    }
+  }
+
   render() {
-    const { achievements, error } = this.props;
+    const { achievements, ageLevelDataSource, error } = this.props;
     if (achievements !== null) {
       return (
-        <Text>{achievements.timestamp}</Text>
+        <View style={{flex: 1, width: Dimensions.get("window").width}}>
+          <Navigator initialRoute={{name: "agelevels"}}
+                     renderScene={(route, navigator) => this.renderScene(route, navigator)}/>
+        </View>
       );
     } else if (error !== null) {
       return (
@@ -40,19 +130,10 @@ class Achievements extends Component {
   }
 
   componentDidMount() {
-    console.log("props", this.props);
     if (this.props.achievements === null) {
       this.fetchAchievements();
     }
   }
-}
-
-function isEmpty(obj) {
-  for(var key in obj) {
-    if(obj.hasOwnProperty(key))
-      return false;
-  }
-  return true;
 }
 
 const actions = {
@@ -63,12 +144,23 @@ const actions = {
   setError: (error) => ({
     type: "SET_ACHIEVEMENTS_ERROR",
     error: error
+  }),
+  selectAgelevel: (agelevel) => ({
+    type: "SELECT_AGELEVEL",
+    agelevel: agelevel
+  }),
+  selectAchievement: (achievement) => ({
+    type: "SELECT_ACHIEVEMENT",
+    achievement: achievement
   })
 };
 
 export default connect(state => ({
   achievements: state.achievements.achievements,
-  error: state.achievements.error
+  error: state.achievements.error,
+  ageLevelDataSource: state.achievements.ageLevelDataSource,
+  achievementsDataSource: state.achievements.achievementsDataSource,
+  achievement: state.achievements.achievement
 }), (dispatch) => ({
   actions: bindActionCreators(actions, dispatch)
 }))(Achievements);
--- RoihuApp/components/achievements/reducers.js
+++ RoihuApp/components/achievements/reducers.js
@@ -1,10 +1,27 @@
+'use strict';
+import React, {
+  ListView
+} from 'react-native';
+
+const agelevelComparator = (a, b) => a.localeCompare(b);
+
 export const achievements = (
-  state = { achievements: null,
-            error: null},
+  state = {
+    achievements: null,
+    error: null,
+    ageLevelDataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id}),
+    achievementsDataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1.sort_no !== r2.sort_no})
+  },
   action) => {
     switch (action.type) {
     case "SET_ACHIEVEMENTS":
-      return Object.assign({}, state, {achievements: action.achievements});
+      return Object.assign({}, state, {achievements: action.achievements,
+                                       ageLevelDataSource:
+                                       state.ageLevelDataSource.cloneWithRows(action.achievements.agelevels.sort(agelevelComparator))});
+    case "SELECT_AGELEVEL":
+      return Object.assign({}, state, {achievementsDataSource: state.achievementsDataSource.cloneWithRows(action.agelevel.achievements)});
+    case "SELECT_ACHIEVEMENT":
+      return Object.assign({}, state, {achievement: action.achievement});
     case "SET_ACHIEVEMENTS_ERROR":
       return Object.assign({}, state, {error: action.error});;
     }
