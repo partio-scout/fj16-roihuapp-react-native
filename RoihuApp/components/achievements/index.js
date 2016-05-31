768cd5dbf5d4f4ea581bd6e28e9734d16c6677cf
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-05-31 12:26:43 +0300

    Don't pass in access token if one is not available when fetching achievements

--- RoihuApp/components/achievements/index.js
+++ RoihuApp/components/achievements/index.js
@@ -224,7 +224,7 @@ class Achievements extends Component {
     fetchData("Fetching achievements",
               this.props.actions.setFetchStatus,
               "/AchievementCategories/Translations",
-              {access_token: this.props.credentials.token},
+              this.props.credentials ? {access_token: this.props.credentials.token} : {},
               this.props.actions.setAchievements,
               this.props.lang,
               "Aktiviteettien haku epäonnistui");
