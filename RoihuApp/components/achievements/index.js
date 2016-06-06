0eff1508d084f831ef33f7267bf2a09376c64fdc
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-06-06 22:52:06 +0300

    Add last modified timestamp for selected achievement

--- RoihuApp/components/achievements/index.js
+++ RoihuApp/components/achievements/index.js
@@ -124,6 +124,9 @@ class Achievements extends Component {
         <Text style={achievementStyles.selectedAchievementTitle}>{achievement.title}</Text>
         <View style={achievementStyles.titleSeparator}></View>
         <Text style={achievementStyles.bodyText}>{achievement.bodytext}</Text>
+        <Text style={[categoryStyles.smallText, categoryStyles.textColor]}>
+          {t("Viimeksi muokattu", this.props.lang)} {moment(achievement.last_modified).format(t("Timestamp", this.props.lang))}
+        </Text>
         {this.renderMarkDone(achievement)}
       </View>
     );
