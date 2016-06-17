516b0c4bad01d1821b227525116e6fede6a9d9aa
Sakari Kouti <skouti2@users.noreply.github.com>
2016-06-17 20:26:15 +0300

    Removed duplicate Suomen Partiolaiset

--- RoihuApp/components/settings/index.js
+++ RoihuApp/components/settings/index.js
@@ -39,7 +39,7 @@ class Settings extends Component {
         </View>
         <View style={categoryStyles.articleContentContainer}>
           <Text style={categoryStyles.textColor}>
-            {DeviceInfo.getVersion()}{"\n"}{"\n"}{t("Versiotieto-content", this.props.lang)}{"\n"}{"\n"}Suomen Partiolaiset - Finlands Scouter ry
+            {DeviceInfo.getVersion()}{"\n"}{"\n"}{t("Versiotieto-content", this.props.lang)}
           </Text>  
         </View>                 
         <View style={categoryStyles.articleTitleContainer}>
