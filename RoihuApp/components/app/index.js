85d0703e322395a5ac08500729b0c4feedf435e6
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-07-05 16:50:40 +0300

    Add migration that moves from article view to category if article not present
    This is to compensate removal of "article" key (and rename to
    selectedArticle), when current view in instructions or locations was
    article.

--- RoihuApp/components/app/index.js
+++ RoihuApp/components/app/index.js
@@ -14,11 +14,11 @@ import migrate from 'redux-storage-decorator-migrate';
 import {reducer} from './reducers.js';
 import Navigation from '../navigation/index.js';
 import { baseUrl } from '../../config';
-import { isEmpty } from '../../utils';
+import { isEmpty, last } from '../../utils';
 
 console.log("Using base url", baseUrl);
 
-const engine = migrate(createEngine('roihu'), 1);
+const engine = migrate(createEngine('roihu'), 2);
 engine.addMigration(1, (state) => {
   console.log("migration 1: remove article key from state.instructions and state.locations");
   if (!isEmpty(state.instructions.article)) {
@@ -30,6 +30,15 @@ engine.addMigration(1, (state) => {
   delete state.instructions.article;
   delete state.locations.article;
   return state;
+});
+engine.addMigration(2, (state) => {
+  console.log("migration 2: when selected article is null and current view is article, change view to category");
+  if (last(state.instructions.routeStack).name === "article" && !state.instructions.selectedArticle) {
+    state.instructions.routeStack.pop();
+  }
+  if (last(state.locations.routeStack).name === "article" && !state.locations.selectedArticle) {
+    state.locations.routeStack.pop();
+  }
 });
 const middleware = storage.createMiddleware(engine);
 const createStoreWithMiddleware = applyMiddleware(middleware)(createStore);
