f87e62f413276e87ea1704610ce2f7eebf207511
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-07-10 21:57:34 +0300

    Group events by date, add (dysfunctional) buttons for changing date

--- RoihuApp/components/calendar/reducers.js
+++ RoihuApp/components/calendar/reducers.js
@@ -2,11 +2,21 @@ import React, {
   ListView
 } from 'react-native';
 import { sortByDate } from '../../utils';
+const R = require('ramda');
+const moment = require('moment');
+
+const partitionKey = (timestamp) => timestamp.format("YYYY.MM.DD");
+
+const partitionEventsByDay = (events) => R.groupBy((event) => partitionKey(moment(event.startTime, moment.ISO_8601)), events);
+
+const sortByStartTime = (a, b) => sortByDate(a.startTime, b.startTime);
 
 export const calendar = (
   state = {routeStack: [{name: "calendar-root"}],
            calendar: null,
            event: null,
+           eventsByDay: null,
+           selectedDay: null,
            calendarDataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1.eventId !== r2.eventId}),
            error: null},
   action) => {
@@ -19,12 +29,20 @@ export const calendar = (
       return Object.assign({}, state, {routeStack: newStack});
     case "RESET_CALENDAR_ROUTES":
       return Object.assign({}, state, {routeStack: [action.route]});
-
-    case "SET_CALENDAR":
+    case "SET_CALENDAR": {
+      const eventsByDay = partitionEventsByDay(action.calendar.events);
+      const sortedDays = R.sort(sortByDate, Object.keys(eventsByDay));
+      const today = partitionKey(moment());
+      const currentSelectedDay = state.selectedDay ?
+              R.find((date) => date === state.selectedDay, sortedDays) || sortedDays[0] :
+              R.find((date) => date === today, sortedDays) || sortedDays[0];
       return Object.assign({},
                            state,
-                           {calendar: action.calendar.calendar,
-                            calendarDataSource: state.calendarDataSource.cloneWithRows(action.calendar.calendar.events.sort((a, b) => sortByDate(a.startTime, b.startTime)))});
+                           {calendar: action.calendar,
+                            eventsByDay: eventsByDay,
+                            selectedDay: currentSelectedDay,
+                            calendarDataSource: state.calendarDataSource.cloneWithRows(eventsByDay[currentSelectedDay].sort(sortByStartTime))});
+    }
     case "SELECT_CALENDAR_EVENT":
       return Object.assign({},
                            state,
