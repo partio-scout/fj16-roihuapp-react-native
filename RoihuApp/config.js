8e6cd26b0078b5d3b0f7f272cb53246bbefabd20
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2016-09-10 08:24:34 +0300

    Document how baseUrl should be used from emulator/simulator and on device

--- RoihuApp/config.js
+++ RoihuApp/config.js
@@ -1,4 +1,6 @@
-export const baseUrl = "https://app-dev.roihu2016.fi";
+// Change address to one that is visible to Android emulator (ex. 10.0.2.2) or iOS simulator (localhost)
+// Change accordingly if running from a real device
+export const baseUrl = "http://10.0.2.2:3000";
 
 export const config = {
   apiUrl: baseUrl + "/api",
